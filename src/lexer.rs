cfb5b94a13f99f410bc86e6f4c66fe6197cf54ae
royboot <rgrizell467@gmail.com>
2025-06-11 02:02:53 +0200

    began work on lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,14 @@
-use logos::Logos;
+use std::ffi::c_void;
+use logos::{Logos};
+#[derive(Logos, Debug, PartialEq)]
+//This is the tokenizer its goal is to take a stream of code into a stream of tokens we can use 
 pub enum Token {
-    
+    #[regex(r"\d+")] Number,
+    #[token("+")] Plus,
+    #[token("-")] Minus,
+    #[token("*")] Mul,
+    #[token("/")] Div,
+    #[token("%")] Mod,
+}
+fn test() {
 }
