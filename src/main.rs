d1f57bb8769f559e099854dc0c7bc632df93868c
royboot <rgrizell467@gmail.com>
2025-06-15 14:27:58 +0200

    added test to test the lexer

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use crate::lexer::Token;
+
 mod lexer;
 
 fn main() {
-    println!("Hello, world!");
+    let input = "123+456-200";
+    let mut lexer = lexer::Lexer::new(input);
+    loop{
+        let token = lexer.next_token();
+        println!("{:?}", token);
+        if token == Token::EOF{
+            break;
+        }
+    }
 }
