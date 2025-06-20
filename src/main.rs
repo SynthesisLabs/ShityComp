c2ba11d22d02a83698da61c93837f5d03a885ce4
royboot <rgrizell467@gmail.com>
2025-06-20 18:18:23 +0200

    moved new test to test.rs file

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,5 @@ mod test;
 
 fn main() {
     let input = "123+456-200";
-    test_lexer(input)
-}
-fn test_lexer(input: &str){
-    //initialize lexer
-    let mut lexer = lexer::Lexer::new(input);
-    loop{
-        let token = lexer.next_token();
-        println!("{:?}", token);
-        if token == Token::EOF||token == Token::Err{
-            break;
-        }
-    }
-    
+    test::test_lexer(input)
 }
--- src/test.rs
+++ src/test.rs
@@ -1,1 +1,16 @@
+use crate::lexer;
+use crate::lexer::Token;
+
 //file containing all test methods
+pub fn test_lexer(input: &str){
+    //initialize lexer
+    let mut lexer = lexer::Lexer::new(input);
+    loop{
+        let token = lexer.next_token();
+        println!("{:?}", token);
+        if token == Token::EOF||token == Token::Err{
+            break;
+        }
+    }
+
+}
