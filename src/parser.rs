d06abfa604f4834386a3fd0a64d5f131428abb7c
royboot <rgrizell467@gmail.com>
2025-07-01 23:42:27 +0200

    added custom enum for nodeTypes and implemented a way to easily log them

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,36 @@
 
+use std::fmt;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+enum NodeType {
+    NumericLiteral,
+    String,
+    Program,
+}
 
 struct NumericLiteral{
-    node_type: String,
+    node_type: NodeType,
     value: i64,
+}
+struct Program{
+    node_type: NodeType,
+    value : NumericLiteral,
 }
 
 pub struct Parser{
     input: String,
+}
+impl fmt::Display for NodeType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let name = match self {
+            NodeType::NumericLiteral => "NumericLiteral",
+            NodeType::Program => "Program",
+            _ => {
+                return Err(fmt::Error);
+            }
+        };
+        write!(f, "{}", name)
+    }
 }
 impl Parser{
     pub fn new(input: &str) -> Parser{
@@ -20,12 +44,12 @@ impl Parser{
     pub fn program(&mut self){
         self.numeric_literal();
         println!("Number of literals: {}", self.input.len());
-        println!("Node type: {}",self.numeric_literal().node_type);
+        println!("Node type: {:?}",self.numeric_literal().node_type);
         println!("Value: {}", self.numeric_literal().value) 
     }
     fn numeric_literal(&mut self)-> NumericLiteral{
         NumericLiteral{
-            node_type: "NumericLiteral".to_string(),
+            node_type: NodeType::NumericLiteral,
             value: self.input.trim().parse::<i64>().unwrap(),
         }
     }
